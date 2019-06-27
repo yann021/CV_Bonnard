b0902f90bf9f1ad1f6083f4c5467210dda171e23
yann021 <y.bonnard4@gmail.com>
2019-06-27 12:02:48 -0700

    create routes for contact

--- config/routes.rb
+++ config/routes.rb
@@ -2,12 +2,16 @@ Rails.application.routes.draw do
 
   root to: 'homes#index'
 
-
   resources :developements
-  get 'contacts/show'
+
+  resources :contact, only: [:new, :create]
+
   resources :trainings
+
   resources :experiences
+
   resources :homes
+
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
