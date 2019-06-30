d0e52623ebfb3d3b081dbf350f437072aa3bae43
yann021 <y.bonnard4@gmail.com>
2019-06-30 07:01:36 -0700

    def create in avaexpe controller

--- app/controllers/avaexpes_controller.rb
+++ app/controllers/avaexpes_controller.rb
@@ -1,2 +1,7 @@
 class AvaexpesController < ApplicationController
+  def create
+    @experience = User.find(params[:experience_id])
+    @experience.avaexpe.attach(params[:avaexpe])
+    redirect_to(user_path(@experience))
+  end
 end
