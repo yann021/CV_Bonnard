140f0e833bcd141cfc3b56dd3247ab950d3938dc
yann021 <y.bonnard4@gmail.com>
2019-06-30 07:17:34 -0700

    def create in avadev controller

--- app/controllers/avatrains_controller.rb
+++ app/controllers/avatrains_controller.rb
@@ -2,6 +2,6 @@ class AvatrainsController < ApplicationController
   def create
     @training = User.find(params[:training_id])
     @training.avatrain.attach(params[:avatrain])
-      redirect_to(user_path(@training))
+    redirect_to(user_path(@training))
   end
 end
