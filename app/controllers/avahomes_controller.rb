724b4fd5220c74056c6d416afc3554238fbe3262
yann021 <y.bonnard4@gmail.com>
2019-06-30 06:49:29 -0700

    def create in avahome controller

--- app/controllers/avahomes_controller.rb
+++ app/controllers/avahomes_controller.rb
@@ -1,2 +1,7 @@
 class AvahomesController < ApplicationController
+  def create
+    @home = Home.find(params[:home_id])
+    @home.avahome.attach(params[:avahome])
+    redirect_to(user_path(@home))
+  end
 end
